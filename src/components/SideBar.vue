50f6b9109d644e8925df9b13c702410a22462119
Cmma16 <1cmadvincula@gmail.com>
2023-01-26 14:11:19 +0800

    Imported my html codes from my previous html file

---
+++ src/components/SideBar.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+
+<template>
+  <div
+    class="left-sidebar mt-12 sticky top-12 z-50 bg-white mx-1 max-h-[470px] hidden md:flex flex-col p-2 rounded-md shadow-sm"
+  >
+    <div class="m-5 flex flex-col">
+      <button class="material-symbols-outlined flex self-end">Settings</button>
+      <div class="flex flex-col bg-[#4DEC9A] p-4 mt-2 rounded-md pr-14">
+        <div class="flex flex-row items-center">
+          <img
+            src="/img/userprofile.jpeg"
+            class="rounded-full max-h-20 aspect-square"
+          />
+          <h1 class="font-medium text-2xl ml-2">@USERNAME</h1>
+        </div>
+        <span class="font-normal text-base mt-2">Name</span>
+        <p class="font-medium text-xl">Your Name Here</p>
+        <span class="font-normal text-base mt-2">Phone Number</span>
+        <p class="font-medium text-xl">0912-345-6789</p>
+        <span class="font-normal text-base mt-2">Email Address</span>
+        <p class="font-medium text-xl">YourEmail@email.com</p>
+      </div>
+      <button class="rounded-full bg-[#00A651] h-10 m-3">
+        Create New Report
+      </button>
+    </div>
+  </div>
+</template>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,52 @@
+<script>
+import NavBar from "../components/NavBar.vue";
+import Posts from "../components/Posts.vue";
+import SideBar from "../components/SideBar.vue";
+import CreateReport from "../components/CreateReport.vue";
+
+export default {
+  data() {
+    return {
+      count: 3,
+    };
+  },
+  components: {
+    Posts,
+    NavBar,
+    SideBar,
+    CreateReport,
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="body flex bg-[#F4F4F4]">
+    <div class="mx-auto flex flex-row">
+      <div class="flex flex-col items-stretch">
+        <NavBar />
+        <div v-for="i in count" :key="i">
+          <Posts />
+        </div>
+        <CreateReport />
+      </div>
+      <SideBar />
+    </div>
+  </div>
+</template>
+
+<!--<div class="posts">
+    <h1>Posts</h1>
+
+    <ul>
+      <li>
+        <RouterLink to="/postDetail/id1">Post 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/id2">Post 2</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/id3">Post 3</RouterLink>
+      </li>
+    </ul>
+  </div>-->
