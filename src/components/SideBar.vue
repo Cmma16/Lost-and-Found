64b40846630a346aeae6a2295b2ba5cbae13a2a6
Cmma16 <1cmadvincula@gmail.com>
2023-02-02 12:37:43 +0800

    Added functions to login/register forms + design

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,11 +1,53 @@
-<script setup></script>
+<script>
+import { useRoute, useRouter } from "vue-router";
+import { useStore } from "vuex";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { onMounted } from "vue";
+import { ref } from "vue";
+import { async } from "@firebase/util";
+
+export default {
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+    const store = useStore();
+    const auth = getAuth();
+    const user_email = ref("");
+    const number = ref("");
+    const uid = ref("");
+
+    onMounted(async () => {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          uid.value = user.uid;
+        }
+      });
+    });
+
+    const goToLogin = () => {
+      router.push({ name: "home" });
+    };
+
+    return {
+      user_email,
+      number,
+      uid,
+    };
+  },
+};
+</script>
 
 <template>
   <div
-    class="left-sidebar mt-12 sticky top-12 z-50 bg-white mx-1 max-h-[470px] hidden md:flex flex-col p-2 rounded-md shadow-sm"
+    class="left-sidebar mt-14 sticky top-12 z-50 bg-white mx-1 max-h-[470px] hidden md:flex flex-col p-2 rounded-md shadow-sm"
   >
     <div class="m-5 flex flex-col">
-      <button class="material-symbols-outlined flex self-end">Settings</button>
+      <button
+        class="material-symbols-outlined flex self-end"
+        @click="$store.dispatch('logout')"
+      >
+        Settings
+      </button>
       <div class="flex flex-col bg-[#4DEC9A] p-4 mt-2 rounded-md pr-14">
         <div class="flex flex-row items-center">
           <img
@@ -17,9 +59,17 @@
         <span class="font-normal text-base mt-2">Name</span>
         <p class="font-medium text-xl">Your Name Here</p>
         <span class="font-normal text-base mt-2">Phone Number</span>
-        <p class="font-medium text-xl">0912-345-6789</p>
+        <input
+          class="font-medium text-xl bg-transparent pointer-events-none"
+          type="text"
+          v-model="number"
+        />
         <span class="font-normal text-base mt-2">Email Address</span>
-        <p class="font-medium text-xl">YourEmail@email.com</p>
+        <input
+          class="font-medium text-xl bg-transparent pointer-events-none"
+          type="text"
+          v-model="user_email"
+        />
       </div>
       <RouterLink to="/create-report" class="flex flex-col">
         <button class="rounded-full bg-[#00A651] h-10 m-3">
